ec99362bee69412f1fa49f99d7a2ee3e17fd94a1
Evgeniya Karabanova <karabanovaep@yandex.ru>
2022-06-03 19:16:52 +0400

    feat: add styles for responsive design, eventListeners for main buttons

--- src/js/index.js
+++ src/js/index.js
@@ -4,37 +4,59 @@ const library = document.querySelector('.library');
 const book = document.querySelector('.book');
 const add = document.querySelector('.add');
 const addBtns = document.querySelectorAll('.header__btn-add, .library__add-btn');
-const backBtn = document.querySelector('.header__btn_back');
+const backBtn = document.querySelectorAll('.header__btn_back');
+const btnSearch = document.querySelectorAll('.header__btn_search');
+const search = document.querySelector('.search');
+
 
 const router = new Navigo('/', {
-	hash: true,
+  hash: true,
 });
 
 const closeAllPage = () => {
-	library.classList.add('hidden');
-	book.classList.add('hidden');
-	add.classList.add('hidden');
+  library.classList.add('hidden');
+  book.classList.add('hidden');
+  add.classList.add('hidden');
 }
 
-router.on( {
-	'/': () => {
-		closeAllPage();
-		library.classList.remove('hidden');
-	},
-	'book': () => {
-		closeAllPage();
-		book.classList.remove('hidden');
-	},
-	'add': () => {
-		closeAllPage();
-		add.classList.remove('hidden');
-	},
+router.on({
+  '/': () => {
+    closeAllPage();
+    library.classList.remove('hidden');
+  },
+  'book': () => {
+    closeAllPage();
+    book.classList.remove('hidden');
+  },
+  'add' : () => {
+    closeAllPage();
+    add.classList.remove('hidden');
+  }
 }).resolve();
 
- addBtns.forEach(btn => {
-	 btn.addEventListener('click', () => {
-		 router.navigate('add');
-	 })
- });
+addBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    router.navigate('add');
+  })
+});
 
+backBtn.forEach(btn => {
+  btn.addEventListener('click', () => {
+    router.navigate('/');
+  })
+});
 
+const closeSearch = ({target}) => {
+  if (target.closest('.search, .header__btn_search')) {
+    return;
+  }
+  search.classList.remove('search_active');
+  document.body.removeEventListener('click', closeSearch);
+};
+
+btnSearch.forEach(btn => {
+  btn.addEventListener('click', () => {
+    search.classList.add('search_active');
+    document.body.addEventListener('click', closeSearch);
+  });
+});
